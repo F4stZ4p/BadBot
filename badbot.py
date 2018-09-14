7bcb33174eb2ee56796eaa4e6e5baa653fe317be
F4stZ4p <37242692+F4stZ4p@users.noreply.github.com>
2018-09-14 20:21:29 +0300

    Update badbot.py

--- badbot.py
+++ badbot.py
@@ -9,11 +9,10 @@ async def ping(ctx):
     await ctx.send('Pong!')
 
 if __name__ == "__main__":
-    for extension in bot.extensions:
-        try:
-            bot.load_extension('modules.antilink')
-            bot.load_extension('modules.pingmodule')
-        except Exception as e:
-            print(e)
+    try:
+        bot.load_extension('modules.antilink')
+        bot.load_extension('modules.pingmodule')
+    except Exception as e:
+        print(e)
 
 bot.run(os.getenv('TOKEN'))
