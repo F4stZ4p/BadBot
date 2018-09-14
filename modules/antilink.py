697a67274701a29d10e98deea789061b3a358dcb
F4stZ4p <37242692+F4stZ4p@users.noreply.github.com>
2018-09-14 20:41:27 +0300

    Update antilink.py

--- modules/antilink.py
+++ modules/antilink.py
@@ -4,10 +4,17 @@ from discord.ext import commands
 class Antilink():
     def __init__(self, bot):
         self.bot = bot
+        
+    async def __delete_link(self, message):
+        if message.content.startswith('https') or message.content.startswith('http') or message.content.startswith('discord.gg') or message.content.startswith('www'):
+            try:
+                await message.delete()
+                await ctx.send(f':link: | **{message.author.mention}, no links allowed here!**', delete_after=5)
+            except:
+                pass
 
     async def on_message(self, message):
-        if message.content.startswith('https') or message.content.startswith('http') or message.content.startswith('discord.gg') or message.content.startswith('www'):
-            await message.delete()
+        await self.__delete_link(message)
 
 def setup(bot):
     bot.add_cog(Antilink(bot))
