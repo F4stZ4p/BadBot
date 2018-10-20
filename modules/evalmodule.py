ba493d841c7e03cd3c672b0efecfaca03620de69
F4stZ4p <37242692+F4stZ4p@users.noreply.github.com>
2018-10-20 16:04:24 +0300

    Update evalmodule.py

--- modules/evalmodule.py
+++ modules/evalmodule.py
@@ -26,7 +26,7 @@ class EvalModule():
         return f"""
 class Context():
     def __init__(self):
-        self.bot = {ctx.me.display_name}
+        self.message_id = {ctx.message.id}
         self.discriminator = {ctx.author.discriminator}
     def __repr__(self):
         return 'Context Object'
