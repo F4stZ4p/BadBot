8877f76af3008395d8023e0f19bf02f4e6e4f722
F4stZ4p <37242692+F4stZ4p@users.noreply.github.com>
2018-10-20 22:04:25 +0300

    Update evalmodule.py

--- modules/evalmodule.py
+++ modules/evalmodule.py
@@ -30,12 +30,16 @@ class MessageAuthor():
         self.name = '{ctx.author.name}'
     def __repr__(self):
         return '{ctx.author.name}#{ctx.author.discriminator}'
+    def __setattr__(self, name, value):
+        raise RuntimeError('Can\'t set an attribute')
 
 class Context():
     def __init__(self):
         self.author = MessageAuthor()
     def __repr__(self):
         return 'Context Object'
+    def __setattr__(self, name, value)
+        raise RuntimeError('Can\'t set an attribute')
 
 ctx = Context()
 {self.cleanup_code(code)}
