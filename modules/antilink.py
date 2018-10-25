25c8ff3c1c13f25f8ad743c126390fd91392154e
F4stZ4p <37242692+F4stZ4p@users.noreply.github.com>
2018-10-25 20:31:04 +0300

    Update antilink.py

--- modules/antilink.py
+++ modules/antilink.py
@@ -1,16 +1,18 @@
 import discord
+import re
 from discord.ext import commands
 
 class Antilink():
     def __init__(self, bot):
         self.bot = bot
+        self._link_regex = re.compile(r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+')
         
     async def delete_link(self, message):
         if not message.channel:
             return
         if message.author == self.bot.user:
             return
-        if message.content.startswith('https') or message.content.startswith('http') or message.content.startswith('discord.gg') or message.content.startswith('www'):
+        if re.match(message, self._link_regex):
             try:
                 await message.delete()
                 await message.channel.send(f':link: | **{message.author.mention}, no links allowed here!**', delete_after=5)
